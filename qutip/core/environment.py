6de99f60b3c25b2e6226fc4fa2ea30f593a47ead
mcditooss <gerardofingurbe@gmail.com>
2024-10-09 15:25:09 +0200

    Removed some 1/T problems in Underdamped the same needs to be done for the others

--- qutip/core/environment.py
+++ qutip/core/environment.py
@@ -1183,31 +1183,35 @@ class UnderDampedEnvironment(BosonicEnvironment):
         result = ExponentialBosonicEnvironment(
             *lists, T=self.T, combine=combine, tag=tag)
         return result
-
+    #TODO: FIX T=0 in other envs 
     def _matsubara_params(self, Nk):
         """ Calculate the Matsubara coefficients and frequencies. """
-        if self.T==0:
-            beta=np.Inf
-        else:    
-            beta = 1 / self.T
         Om = np.sqrt(self.w0**2 - (self.gamma / 2)**2)
         Gamma = self.gamma / 2
-
-        ck_real = ([
-            (self.lam**2 / (4 * Om))
-            * (1 / np.tanh(beta * (Om + 1j * Gamma) / 2)),
-            (self.lam**2 / (4 * Om))
-            * (1 / np.tanh(beta * (Om - 1j * Gamma) / 2)),
-        ])
+        if self.T==0:
+            beta=np.Inf
+            ck_real = ([
+                (self.lam**2 / (4 * Om)),
+                (self.lam**2 / (4 * Om)),
+            ])
+        else:    
+            beta = 1 / self.T
+            ck_real = ([
+                (self.lam**2 / (4 * Om))
+                * (1 / np.tanh(beta * (Om + 1j * Gamma) / 2)),
+                (self.lam**2 / (4 * Om))
+                * (1 / np.tanh(beta * (Om - 1j * Gamma) / 2)),
+            ])
 
         ck_real.extend([
-            (-2 * self.lam**2 * self.gamma / beta) * (2 * np.pi * k / beta)
+            (-2 * self.lam**2 * self.gamma *self.T) * (2 * np.pi * k *self.T)
             / (
-                ((Om + 1j * Gamma)**2 + (2 * np.pi * k / beta)**2)
-                * ((Om - 1j * Gamma)**2 + (2 * np.pi * k / beta)**2)
+                ((Om + 1j * Gamma)**2 + (2 * np.pi * k *self.T)**2)
+                * ((Om - 1j * Gamma)**2 + (2 * np.pi * k *self.T)**2)
             )
             for k in range(1, Nk + 1)
         ])
+        print(ck_real)
 
         vk_real = [-1j * Om + Gamma, 1j * Om + Gamma]
         vk_real.extend([
